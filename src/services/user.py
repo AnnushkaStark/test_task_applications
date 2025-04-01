6d207e10dfa84e2501aef1a142a1c8b841693f82
root <psk221219@gmail.com>
2025-04-02 02:27:24 +0300

    update services

--- src/services/user.py
+++ src/services/user.py
@@ -3,10 +3,10 @@ from typing import Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from crud.user import user_crud
-from services import verify as verification_service
 from models import User
 from schemas.user import UserCreate, UserLogin
 from schemas.verify import VerificationCodeCreate
+from services import verify as verification_service
 from utilities.email_client import email_client
 from utilities.security.password_hasher import (
     get_password_hash,
@@ -26,8 +26,12 @@ async def create(db: AsyncSession, create_data: UserCreate) -> Optional[User]:
         db=db, create_schema=create_data.model_dump()
     )
     verify_code = await generate_veriify_code()
-    verification_code_schema = VerificationCodeCreate(verification_code=verify_code)
-    await verification_service.create(db=db, create_schema=verification_code_schema, user_id=user.id)
+    verification_code_schema = VerificationCodeCreate(
+        verification_code=verify_code
+    )
+    await verification_service.create(
+        db=db, create_schema=verification_code_schema, user_id=user.id
+    )
     await email_client.send_mail(recepients=[user.email], body=verify_code)
     return user
 
@@ -44,5 +48,11 @@ async def login(user: User, login_schema: UserLogin) -> dict:
     )
     return await create_tokens(subject)
 
-async def verify_account(user: User, verification_code: str) -> None:
 
+async def verify_account(
+    db: AsyncSession, user_id: int, verification_code: str
+) -> None:
+    user = await user_crud.get_by_id(db=db, obj_id=user_id)
+    if user.verify_code == verification_code:
+        await user_crud.mark_verify(db=db, db_obj=user)
+    raise Exception("Invalid verify code")
--- src/services/verify.py
+++ src/services/verify.py
@@ -1,10 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from models import VerificationCode
-from schemas.verify import VerificationCodeCreate, VerificationCodeCreateDB
+
 from crud.verify import verification_code_crud
+from schemas.verify import VerificationCodeCreate, VerificationCodeCreateDB
 
 
-async def create(db: AsyncSession, create_schema: VerificationCodeCreate, user_id: int):
-    create_data = VerificationCodeCreateDB(**create_schema.model_dump(), user_id=user_id)
-    code = await verification_code_crud.create(db=db, create_schema=create_data)
-    return code
+async def create(
+    db: AsyncSession, create_schema: VerificationCodeCreate, user_id: int
+) -> None:
+    create_data = VerificationCodeCreateDB(
+        **create_schema.model_dump(), user_id=user_id
+    )
+    await verification_code_crud.create(db=db, create_schema=create_data)
